ded3d48c86042b630af48a5b71a0df8558ac88e5
dholsk31-prog <dholsk31@gmail.com>
2026-01-16 21:20:57 +0530

    Create 00adhar.js

---
+++ Priyansh/commands/00adhar.js
@@ -0,0 +1,29 @@
+//@Prem-babu3608
+////////////////////////////////////////////////////////
+/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT PREM BABU
+const fs = require("fs");
+module.exports.config = {
+	name: "adhar",
+    version: "1.1.1",
+	hasPermssion: 0,
+	credits: "PREM BABU", //////@prem-babu3608
+	description: "THIS BOT IS MR PREM SHARMA",
+	commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("Adhar") ||
+react.includes("aadhar")) {
+		var msg = {
+				body: ``,attachment: fs.createReadStream(__dirname + `/noprefix/aadhar.jpg`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("💾", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+	}
